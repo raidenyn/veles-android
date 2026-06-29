bbd4882f2ec3b480d6caef2a814957e94d6b9269
Yuriy Nagaev <yuriy@nagaev.me>
2026-06-29 23:24:05 +0700

    build: add Room 2.6.1 and KSP 2.1.10-1.0.31 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -84,6 +85,9 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.window)
 
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
     testImplementation(libs.mockk.android)
