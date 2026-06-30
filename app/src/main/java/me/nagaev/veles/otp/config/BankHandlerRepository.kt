5f7aa0c6f5484c45540f42220912c406ba851ba3
Yuriy Nagaev <yuriy@nagaev.me>
2026-06-30 23:44:01 +0700

    feat: add suspend CRUD methods to DAO and Repository

--- app/src/main/java/me/nagaev/veles/otp/config/BankHandlerRepository.kt
+++ app/src/main/java/me/nagaev/veles/otp/config/BankHandlerRepository.kt
@@ -6,4 +6,12 @@ class BankHandlerRepository(context: Context) {
     private val dao = BankHandlerDatabase.getInstance(context).bankHandlerConfigDao()
 
     fun getAll(): List<BankHandlerConfig> = dao.getAll()
+
+    suspend fun getAllSuspend(): List<BankHandlerConfig> = dao.getAllSuspend()
+
+    suspend fun insert(config: BankHandlerConfig): Long = dao.insert(config)
+
+    suspend fun update(config: BankHandlerConfig) = dao.update(config)
+
+    suspend fun delete(config: BankHandlerConfig) = dao.delete(config)
 }
