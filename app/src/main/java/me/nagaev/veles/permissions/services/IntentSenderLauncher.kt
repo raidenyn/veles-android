ad985bd6ba98cb2414043fd1f63a8047f58a2087
Veles Dev <veles-dev@users.noreply.github.com>
2026-07-12 14:15:21 +0200

    feat: declare companion-watch + sensitive-notifications permissions, IntentSender launcher (#43)

---
+++ app/src/main/java/me/nagaev/veles/permissions/services/IntentSenderLauncher.kt
@@ -0,0 +1,28 @@
+package me.nagaev.veles.permissions.services
+
+import android.app.Activity
+import android.content.IntentSender
+import androidx.activity.ComponentActivity
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+
+class IntentSenderLauncher(
+    val launch: (intentSender: IntentSender, callback: (resultOk: Boolean) -> Unit) -> Unit,
+) {
+    companion object {
+        fun create(activity: ComponentActivity): IntentSenderLauncher {
+            var closerCallback = { _: Boolean -> }
+            val resultLauncher =
+                activity.registerForActivityResult(
+                    ActivityResultContracts.StartIntentSenderForResult(),
+                ) { result ->
+                    closerCallback(result.resultCode == Activity.RESULT_OK)
+                }
+            val launch = { intentSender: IntentSender, callback: (Boolean) -> Unit ->
+                closerCallback = callback
+                resultLauncher.launch(IntentSenderRequest.Builder(intentSender).build())
+            }
+            return IntentSenderLauncher(launch)
+        }
+    }
+}
